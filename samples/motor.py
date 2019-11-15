92cf467591cd398dbac8cb45979b43b967dd8b6b
Yoshikazu YANO <yoshi@hmi.aitech.ac.jp>
2019-11-15 11:46:30 +0900

    Update motor.py
    motor コメント追加

--- samples/motor.py
+++ samples/motor.py
@@ -7,9 +7,10 @@ sw1 = Pin(0, Pin.IN, Pin.PULL_UP)
 mtr = Pin(15, Pin.OUT, value=0)
 brk = Pin( 2, Pin.OUT, value=0)
 
-# 昇圧回路駆動
-pulse = PWM(Pin(23), freq=75000, duty=512)
-
+# 昇圧回路駆動 ブレーキ動作に必要
+pulse = PWM(Pin(23), freq=75000, duty=512
+            
+# ロータリエンコーダ出力値の取得
 sens1 = ADC(Pin(36))
 sens1.atten(ADC.ATTN_6DB)
 sens1.width(ADC.WIDTH_10BIT)
@@ -21,7 +22,7 @@ while True:
     else:
         mtr.on()
 
-    print(sens1.read())
+    print(sens1.read())  # エンコーダの値を表示
 
 
 """
